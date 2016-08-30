14be8588a16c4645113c2aa22b491e6114bd22a6
Luke Josh <lukewjosh@gmail.com>
2016-08-30 15:44:01 +1000

    Add example event display - no styling, just displaying fields of an event object on a page.

--- app/controllers/EventController.java
+++ app/controllers/EventController.java
@@ -1,13 +1,15 @@
 package controllers;
 
+import models.Event;
 import play.mvc.*;
 
 import views.html.*;
-/**
- * Created by luke on 29/08/16.
- */
+
+import java.time.LocalDateTime;
+
 public class EventController extends Controller {
+    private Event testEvent = new Event("Test Event", LocalDateTime.of(2016, 10, 10, 12, 00), "Test Venue", 100, 20.50, new int[3], true);
     public Result event() {
-        return ok("I am an event page");
+        return ok(event.render(testEvent));
     }
 }
