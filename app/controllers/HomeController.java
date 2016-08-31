d2defe2ff7e280803c533aa82da613df8ad9d80f
Luke Josh <lukewjosh@gmail.com>
2016-08-31 18:15:21 +1000

    Begin rewrite of index page. Add title bar Add 'top' event cards

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -17,8 +17,6 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("Community organisation my dudes"));
+        return ok(index.render());
     }
-
-
 }
