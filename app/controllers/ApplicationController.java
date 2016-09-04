4012cf4d8037b12498db5371fc3c919863491383
Luke Josh <lukewjosh@gmail.com>
2016-09-05 03:39:23 +1000

    so many changes omg Registration now works - fields will be sent to the db and a new entry will be created. Add 'field constructor' to make it easier to work with forms and materialize

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -1,28 +1,40 @@
 package controllers;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.*;
 import views.html.*;
 import models.*;
+
+import javax.inject.Inject;
+
 import static play.mvc.Results.ok;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
-public class ApplicationController {
+public class ApplicationController  extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+    private FormFactory formFactory;
+
+    @Inject
+    public ApplicationController(FormFactory formFactory) {
+        this.formFactory = formFactory;
+    }
+
     public Result index() {
-        User user = User.find.byId(1L);
-        System.out.println(user.getEmail());
-        return ok(index.render());
+        Form<User> userForm = formFactory.form(User.class);
+        return ok(index.render(userForm));
     }
 
     public Result event(){
         return ok(event.render());
+    }
+
+    public Result saveUser(){
+        Form<User> userForm = formFactory.form(User.class).bindFromRequest();
+        User test = userForm.get();
+        System.out.println(userForm.toString());
+        return index();
     }
 }
